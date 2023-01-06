4550c9b1a310f635beb838d153d1e1c3a7fb1870
gyueun <103631319+Gyueunn@users.noreply.github.com>
2023-01-06 13:44:36 +0900

    Connected Page

---
+++ choice/lib/question/8.dart
@@ -0,0 +1,188 @@
+import 'package:flutter/material.dart';
+
+import '../func/home.dart';
+import '../func/list.dart';
+import '7.dart';
+import '9.dart';
+
+class Question8 extends StatelessWidget {
+  const Question8({super.key});
+
+  static const String _title = 'Flutter Code Sample';
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: _title,
+      home: MyStatefulWidget(),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({super.key});
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  int current_index = 0;
+  final List<Widget> _children = [Home(), Listview(), Home(), Home()];
+  static const TextStyle optionStyle =
+  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+
+  @override
+  Widget build(BuildContext context) {
+    final deviceHeight = MediaQuery.of(context).size.height;
+    final standardDeviceHeight = 900;
+    final deviceWidth = MediaQuery.of(context).size.width;
+    final standardDeviceWidth = 410;
+
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Expanded(
+              flex: 4,
+              child: Container(
+                color: Colors.white,
+                child: Column(
+                  children: [
+                    Container(
+                      color: Colors.white,
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: IconButton(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 30,
+                              vertical: 30
+                          ),
+                          icon: Image.asset('assets/images/left.png'),
+                          iconSize: 30,
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => Question7()),
+                            );
+                          },
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 40 * ( deviceHeight / standardDeviceHeight),
+                    ),
+                    Container(
+                      color: Colors.white,
+                      child: Center(
+                        child: Image.asset(
+                          'assets/images/bar8.png',
+                          height: 50 * ( deviceWidth / standardDeviceWidth),
+                          width: 350 * ( deviceWidth / standardDeviceWidth),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 40 * ( deviceHeight / standardDeviceHeight),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 8,
+              child: Container(
+                padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                color: Colors.white,
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 120 * ( deviceHeight / standardDeviceHeight),
+                      child: Text('8. 하나님에 대해 더 알아보고 싶은 마음이 있으신가요?',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 30,
+                            color: Colors.black,
+                          ),
+                          textAlign: TextAlign.center
+                      ),
+                    ),
+                    SizedBox(
+                      height: 180 * ( deviceHeight / standardDeviceHeight),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          InkWell(
+                              child: IconButton(
+                                icon: Image.asset('assets/images/1.png'),
+                                iconSize: 160,
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(builder: (context) => Question9()),
+                                  );
+                                },
+                              )
+                          ),
+                          InkWell(
+                              child: IconButton(
+                                icon: Image.asset('assets/images/2.png'),
+                                iconSize: 160,
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(builder: (context) => Question9()),
+                                  );
+                                },
+                              )
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: current_index,
+        backgroundColor: Colors.black,
+        onTap: (index) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => _children[index]),
+          );
+        },
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: '홈',
+            backgroundColor: Colors.black,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.menu),
+            label: '리스트',
+            backgroundColor: Colors.black,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: '찜',
+            backgroundColor: Colors.black,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: '프로필',
+            backgroundColor: Colors.black,
+          ),
+        ],
+        selectedItemColor: Colors.grey,
+        unselectedItemColor: Colors.grey,
+        showUnselectedLabels: true,
+      ),
+    );
+  }
+}
